1b855b218e6011dca50efb54b3eb382f253c19e4
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-12-13 12:38:41 -0500

    otherpj

---
+++ project/js/otherp.js
@@ -0,0 +1,36 @@
+/* import "/css/style.css?t=1702479256277"
+import { DOMselectors } from "/js/dom.js";
+
+const URL = `https://data.cityofnewyork.us/resource/uiay-nctu.json`;
+
+ async function getData(){
+  let response = await fetch(URL);
+  let data = await response.json();
+  console.log(data);
+  data.forEach(a => {
+    DOMselectors.container.insertAdjacentHTML("afterend", `
+    <div class="card">
+    <p><h3 class="organization">${a.orgname}</h3></p>
+   <p><h3 class="avenue">${a.apprfromst}</h3></p> 
+    <p><h3 class="street">${a.appronstre}      </h3></p>
+    <h2 class="theborough">${a.boroughname}</h2>
+  </div>`)}
+      )};
+        ; 
+getData(URL)
+
+  
+
+function clearscreen(){
+  DOMselectors.container.innerHTML= "";
+};
+
+let values = document.querySelectorAll(".brooklyn, .bronx, .manhattan, .queens, .statenisland");
+
+values.forEach((value) => value.addEventListener("click", function(){
+  let type = value.textContent;
+  let newArr = URL.filter((data) => URL.boroughname === type);
+  clearscreen();
+  data();
+  getData(newArr);
+})); */
