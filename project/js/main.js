2558ecc530cc1fc7e5b3658b199608e70b9a90ae
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-12-12 22:36:57 -0500

    1212

--- project/js/main.js
+++ project/js/main.js
@@ -1,18 +1,18 @@
-const apiURL ="http https://swapi.dev/api/starships/9/"
-
-console.log(fetch(apiURL));
-fetch(apiURL)
-.then((response) => response.json())
-.then((data) => console.log(data));
+const apiURL = "http https://swapi.dev/api/starships/9/"
 
 async function getData(x) {
-try {
-    const response = await fetch(x);
-    const data = await response.json();
-    console.log(data);
-    return data;
-} catch (error) {
-    console.log(error);
-}
+    try {
+        const response = await fetch(x);
+        response.then((result) => {
+            async function deeperData() {
+                await fetch(result);
+            };
+            deeperData();
+        });
+        const deepestData = await response.json();
+        console.log(deepestData);
+    } catch (error) {
+        console.log(error);
+    }
 }
 getData(apiURL);
