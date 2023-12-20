36bcc4280af7cb1ece4320514a477730af7d7302
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-12-20 13:12:39 -0500

    12/20

--- project/js/main.js
+++ project/js/main.js
@@ -6,22 +6,21 @@ const apiURL = "https://valorant-api.com/v1/"
 async function getData(url) {
     try {
         const response = await fetch(url);
-        const data = response.json();
-        console.log(data);
+        const data = await response.json();
+        console.log(data.data);
     } catch (error) {
         console.log(error);
     }
 }
-getData(apiURL);
 
 DOMselectors.btn.forEach((button) => button.addEventListener("click", function(){
-    let category = button.textContent.toLowerCase.toString
+    let category = button.textContent.toLowerCase
     let newURL = apiURL + category
     getData(newURL);
 }))
 
-const x = "hello"
+/* const x = "hello"
 let y = DOMselectors.BUTTON.textContent.toLowerCase.toString
 let newestURL = x + y
 console.log(newestURL);
-
+ */
