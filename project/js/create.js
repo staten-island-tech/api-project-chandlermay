a543cceb48dfd11296e9253af24499c4c6536100
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-07 14:12:38 -0500

    fixed all accesibility issues

--- project/js/create.js
+++ project/js/create.js
@@ -7,16 +7,15 @@ function createCard(data) {
     <div class="card">
       <div class="main">
           <h2>${obj.displayName}</h2>
-          <img src="${obj.displayIcon}" class="image">
-          <h4>${obj.role.displayName}</h4>
+          <img alt="${obj.displayName}'s face" src="${obj.displayIcon}" class="image">
+          <h3>${obj.role.displayName}</h3>
       </div>
       <span>
-          <h2>Abilities</h2>
           <ul class="spancontainer"> 
-          <p class="spanText">${obj.abilities[0].displayName}</p> <img class="spanImage" src="${obj.abilities[0].displayIcon}">
-          <p class="spanText">${obj.abilities[1].displayName}</p> <img class="spanImage" src="${obj.abilities[1].displayIcon}">
-          <p class="spanText">${obj.abilities[2].displayName}</p> <img class="spanImage" src="${obj.abilities[2].displayIcon}">
-          <p class="spanText">${obj.abilities[3].displayName}</p> <img class="spanImage" src="${obj.abilities[3].displayIcon}">
+          <p class="spanText">${obj.abilities[0].displayName}</p> <img alt="${obj.displayName}'s Ability 1"class="spanImage" src="${obj.abilities[0].displayIcon}">
+          <p class="spanText">${obj.abilities[1].displayName}</p> <img alt="${obj.displayName}'s Ability 2"class="spanImage" src="${obj.abilities[1].displayIcon}">
+          <p class="spanText">${obj.abilities[2].displayName}</p> <img alt="${obj.displayName}'s Ability 3"class="spanImage" src="${obj.abilities[2].displayIcon}">
+          <p class="spanText">${obj.abilities[3].displayName}</p> <img alt="${obj.displayName}'s Ability 4"class="spanImage" src="${obj.abilities[3].displayIcon}">
           </ul>
       </span>
     </div>
